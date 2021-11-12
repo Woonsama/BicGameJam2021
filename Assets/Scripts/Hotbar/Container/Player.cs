eaa2def50e50d1e939bde28883b746c6b0a3ce84
oeccsy <oeccsy@gmail.com>
2021-11-13 02:03:15 +0900

    update Player.cs

--- Assets/Scripts/Hotbar/Container/Player.cs
+++ Assets/Scripts/Hotbar/Container/Player.cs
@@ -7,11 +7,14 @@ namespace Hotbar.Container
 {
     public class Player : SingletonMonoBase<Player>
     {
+        [HideInInspector]
         public GameObject player;
         public GameObject playerPrefab;
         public void CreatePlayer()
         {
-            player = Instantiate(playerPrefab, transform);
+            int randomNum = Random.Range(0, 4);
+            Vector3 randomPos = new Vector3(randomNum, randomNum, randomNum);
+            player = Instantiate(playerPrefab, randomPos, gameObject.transform.rotation);
         }
 
         public void RemovePlayer()

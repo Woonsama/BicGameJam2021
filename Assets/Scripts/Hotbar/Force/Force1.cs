741fcc1ec2241122191609f053685e45e7fb1f0d
oeccsy <oeccsy@gmail.com>
2021-11-13 03:38:40 +0900

    feat : force 생성

---
+++ Assets/Scripts/Hotbar/Force/Force1.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Force1 : MonoBehaviour
+{
+    private Vector3 firstPos;
+    private Vector3 curPos;
+
+    public Vector3 direction;
+    public float range = 10f;
+    public float power = 10000f;
+    public float speed = 0.1f;
+
+    public void Shoot(Vector3 dir)
+    {
+        direction = dir;
+
+        Destroy(gameObject, 2f);
+    }
+
+    private void Start()
+    {
+        direction = new Vector3(0, 0, 1f);
+        firstPos = transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, speed);
+        curPos = transform.position;
+        if (Vector3.Distance(firstPos, curPos) > range)
+        {
+            Destroy(this.gameObject);
+        }
+        transform.Translate(direction);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        "Trigger".Log();
+        other.gameObject.GetComponent<Rigidbody>().AddForce(direction * power, ForceMode.Impulse);
+    }
+}

6e613e91f93a3da73adb22fe2a1716d9c556e38a
Woonsama <kws4510@naver.com>
2021-11-14 10:44:40 +0900

    [Feat] MainSceneManager 커스텀 에디터 추가

--- Assets/Scripts/Hotbar/Manager/MainSceneManager.cs
+++ Assets/Scripts/Hotbar/Manager/MainSceneManager.cs
@@ -4,23 +4,36 @@ using Hotbar.Pattern;
 using Hotbar.UI;
 using Hotbar.UI.View;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Hotbar.Manager
 {
     public class MainSceneManager : SingletonMonoBase<MainSceneManager>
     {
+        [Header("Inspector Helper")]
+        public bool isShowTutorialView = true;
+        public bool isShowRouteMapView = true;
+
         public async void Awake()
         {
-            //Show Tutorial
-            var tutorialView = await UIManager.Instance.OpenView(UIManager.ViewType.Tutorial);
-            await (tutorialView as UITutorialView).StartTutorial();
+
+            if(isShowTutorialView)
+            {
+                //Show Tutorial
+                var tutorialView = await UIManager.Instance.OpenView(UIManager.ViewType.Tutorial);
+                await (tutorialView as UITutorialView).StartTutorial();
+            }
+
 
             //Set Station Data
             SubwayManager.Instance.InitStation();
 
-            //Show Route Map
-            var routeMapView = await UIManager.Instance.OpenView(UIManager.ViewType.RouteMap);
-            //await (routeMapView as UIRouteMapView).StartAnimation();
+            if(isShowRouteMapView)
+            {
+                //Show Route Map
+                var routeMapView = await UIManager.Instance.OpenView(UIManager.ViewType.RouteMap);
+                //await (routeMapView as UIRouteMapView).StartAnimation();
+            }
 
             //NPC Init
             NPCContainer.Instance.Init();
